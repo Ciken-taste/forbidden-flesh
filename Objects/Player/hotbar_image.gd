5aeb4dd82b43b3e19dd7429a6f82723bd2b1f5e9
oneofthecodersofalltime <112718556+oneofthecodersofalltime@users.noreply.github.com>
2024-07-05 22:24:41 +0300

    broken inventory system implemented

---
+++ Objects/Player/hotbar_image.gd
@@ -0,0 +1,8 @@
+extends Control
+
+@onready var sprite := $Panel/Sprite2D as Sprite2D
+@onready var panel := $Panel as Panel
+
+#func _physics_process(_delta):
+	#Dsprite.scale = panel.size
+	#sprite.position = panel.position
